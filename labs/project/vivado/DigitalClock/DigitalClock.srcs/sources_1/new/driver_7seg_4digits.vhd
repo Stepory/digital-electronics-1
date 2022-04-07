04c03bb09c341df0d73c73d414eeb30646b3c76b
Evaldas <82205924+Stepory@users.noreply.github.com>
2022-04-07 23:27:58 +0200

    Add files via upload

---
+++ labs/project/vivado/DigitalClock/DigitalClock.srcs/sources_1/new/driver_7seg_4digits.vhd
@@ -0,0 +1,94 @@
+------------------------------------------------------------
+--
+-- Driver for 4-digit 7-segment display.
+-- Nexys A7-50T, Vivado v2020.1.1, EDA Playground
+--
+-- Copyright (c) 2020-Present Tomas Fryza
+-- Dept. of Radio Electronics, Brno Univ. of Technology, Czechia
+-- This work is licensed under the terms of the MIT license.
+--
+------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+------------------------------------------------------------
+-- Entity declaration for display driver
+------------------------------------------------------------
+entity driver_7seg_4digits is
+    port(
+        clk     : in  std_logic;
+        reset   : in  std_logic;
+        data0_i : in  std_logic_vector(4 - 1 downto 0);
+        data1_i : in  std_logic_vector(4 - 1 downto 0);
+        data2_i : in  std_logic_vector(4 - 1 downto 0);
+        data3_i : in  std_logic_vector(4 - 1 downto 0);
+        data4_i : in  std_logic_vector(4 - 1 downto 0);
+        data5_i : in  std_logic_vector(4 - 1 downto 0);
+        data6_i : in  std_logic_vector(4 - 1 downto 0);
+        data7_i : in  std_logic_vector(4 - 1 downto 0);
+
+        -- 4-bit input value for decimal points
+        dp_i    : in  std_logic_vector(4 - 1 downto 0);
+        -- Decimal point for specific digit
+        dp_o    : out std_logic;
+        -- Cathode values for individual segments
+        seg_o   : out std_logic_vector(7 - 1 downto 0);
+        -- Common anode signals to individual displays
+        dig_o   : out std_logic_vector(4 - 1 downto 0)
+    );
+end entity driver_7seg_4digits;
+
+------------------------------------------------------------
+-- Architecture declaration for display driver
+------------------------------------------------------------
+architecture Behavioral of driver_7seg_4digits is
+
+    -- Internal clock enable
+    signal s_en  : std_logic;
+    -- Internal 2-bit counter for multiplexing 4 digits
+    signal s_cnt : std_logic_vector(2 - 1 downto 0);
+    -- Internal 4-bit value for 7-segment decoder
+    signal s_hex : std_logic_vector(4 - 1 downto 0);
+
+begin
+    --------------------------------------------------------
+    -- Instance (copy) of clock_enable entity generates 
+    -- an enable pulse every 4 ms
+    clk_en0 : entity work.clock_enable
+        generic map(
+            g_MAX => 400000
+        )
+        port map(
+            clk   => clk,-- WRITE YOUR CODE HERE
+            reset => reset,-- WRITE YOUR CODE HERE
+            ce_o  => s_en
+        );
+
+    --------------------------------------------------------
+    -- Instance (copy) of cnt_up_down entity performs a 2-bit
+    -- down counter
+    bin_cnt0 : entity work.cnt_up_down
+        generic map(
+            g_CNT_WIDTH  => 2
+        )
+        port map(
+            clk      => clk,
+            reset    => reset,
+            en_i     => s_en,
+            cnt_up_i => '0',
+            cnt_o    => s_cnt
+        );
+
+    --------------------------------------------------------
+    -- Instance (copy) of hex_7seg entity performs a 7-segment
+    -- display decoder
+    hex2seg : entity work.hex_7seg
+        port map(
+            hex_i => s_hex,
+            seg_o => seg_o
+        );
+
+
+end architecture Behavioral;
